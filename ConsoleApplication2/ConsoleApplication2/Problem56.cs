73acaa492f05170b50dd33779ed503a6dbf9d059
AlexBarke <alexbarke@air-trak.com>
2016-09-07 12:28:42 -0700

    Added solution to problem 56.

--- ConsoleApplication2/ConsoleApplication2/Problem56.cs
+++ ConsoleApplication2/ConsoleApplication2/Problem56.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,38 @@ namespace ConsoleApplication2
     {
         public static void Execute()
         {
+            int maxSum = 0;
+            int aSoln = 0;
+            int bSoln = 0;
+            BigInteger power;
+            for(int a = 1; a < 100; a++)
+            {
+                for(int b = 1; b < 100; b++)
+                {
+                    power = BigInteger.Pow(a, b);
+                    int sum = ComputeDigitalSum(power);
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        aSoln = a;
+                        bSoln = b;
+                    }
+                }
+            }
+            Console.WriteLine(string.Format("The max digital sum is: {0} with a = {1} and b = {2}", maxSum, aSoln, bSoln));
+            Console.Read();
+        }
 
+        private static int ComputeDigitalSum(BigInteger power)
+        {
+            int sum = 0;
+            var charArr = power.ToString().ToCharArray();
+            foreach(char c in charArr)
+            {
+                int i = (int)char.GetNumericValue(c);
+                sum += i;
+            }
+            return sum;
         }
     }
 }
